3983bd8210a50de631ecf8326251943884c84dac
klirik <wishreacher@gmail.com>
2021-04-24 01:04:50 +0300

    Adding not working systems of workers counting ^^

--- Source/TB/Controllers/TBAIController.cpp
+++ Source/TB/Controllers/TBAIController.cpp
@@ -13,22 +13,9 @@ void ATBAIController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
-	APointOfInterests* Points = Cast<APointOfInterests>(GetPawn());
 	if(AIBehavior !=nullptr)
 	{
 		RunBehaviorTree(AIBehavior);
-		GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
-		
-		if(Points != nullptr)
-		{
-			GetBlackboardComponent()->SetValueAsVector(TEXT("FirstLocation"), GetPawn()->GetActorLocation() + Points->FirstLocation);
-			GetBlackboardComponent()->SetValueAsVector(TEXT("SecondLocation"), GetPawn()->GetActorLocation() + Points->SecondLocation);	
-			GetBlackboardComponent()->SetValueAsVector(TEXT("ThirdLocation"), GetPawn()->GetActorLocation() + Points->ThirdLocation);
-			GetBlackboardComponent()->SetValueAsVector(TEXT("FourthLocation"), GetPawn()->GetActorLocation() + Points->FourthLocation);
-			GetBlackboardComponent()->SetValueAsVector(TEXT("ThirdLocation"), GetPawn()->GetActorLocation() + Points->FifthLocation);	
-		}
-		else UE_LOG(LogTemp, Error, TEXT("cast failed"));
 	}
 }
 
