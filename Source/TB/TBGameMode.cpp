a877cb4eff60fcf2bc5ed90ba96e0cca38f7758d
shkadas <niconico733@gmail.com>
2021-04-25 13:15:22 +0400

    Fix UE4 - now work again / Brigade Mood now indicate proper value

--- Source/TB/TBGameMode.cpp
+++ Source/TB/TBGameMode.cpp
@@ -11,12 +11,7 @@
 
 ATBGameMode::ATBGameMode()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+
 }
 
 void ATBGameMode::BeginPlay()
@@ -26,35 +21,37 @@ void ATBGameMode::BeginPlay()
 
 void ATBGameMode::Tick(float DeltaSeconds)
 {
+    Super::Tick(DeltaSeconds);
+    
 	CheckBrigadeMood();
 }
-
-WorkersMood ATBGameMode::CheckBrigadeMood()
-{
-	TArray<AActor*> WorkersActors;
-	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AEnemyCharacter::StaticClass(), WorkersActors);
-	
-	for(AActor* Actor : WorkersActors)
-	{
-		AEnemyCharacter* Worker = Cast<AEnemyCharacter>(Actor);
-		
-		if(Worker == nullptr)
-		{
-			continue;
-		}			
-		if(Worker->WorkerMood == WorkersMood::Good)
-		{
-			GoodCount++;
-		}
-		if(Worker->WorkerMood == WorkersMood::Normal)
-		{
-			NormalCount++;
-		}
-		if(Worker->WorkerMood == WorkersMood::Bad)
-		{
+    
+ WorkersMood ATBGameMode::CheckBrigadeMood()
+ {	
+ 	TArray<AActor*> WorkersActors;
+ 	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AEnemyCharacter::StaticClass(), WorkersActors);
+ 	
+ 	for(AActor* Actor : WorkersActors)
+ 	{
+ 		AEnemyCharacter* Worker = Cast<AEnemyCharacter>(Actor);
+ 		
+ 		if(Worker == nullptr)
+ 		{
+ 			continue;
+ 		}			
+ 		if(Worker->WorkerMood == WorkersMood::Good)
+ 		{
+ 			GoodCount++;
+ 		}
+ 		if(Worker->WorkerMood == WorkersMood::Normal)
+ 		{
+ 			NormalCount++;
+ 		}
+ 		if(Worker->WorkerMood == WorkersMood::Bad)
+ 		{
 			BadCount++;
-		}
-	}
+ 		}
+ 	}
 
 	int a, b, c;
 
@@ -62,27 +59,29 @@ WorkersMood ATBGameMode::CheckBrigadeMood()
 	b = NormalCount;
 	c = BadCount;
 
-	if ( a > b && a > c )
-	{
-		UE_LOG(LogTemp, Warning, TEXT("GoodCount = %i"), a);
-		MoodResult = WorkersMood::Good;
-	}			
-	else if ( b > a && b > c )
-	{
-		UE_LOG(LogTemp, Warning, TEXT("NormalCount = %i"), b);
-		MoodResult = WorkersMood::Normal;
-	}		
-	else if ( c > a && c > b )
-	{
-		UE_LOG(LogTemp, Warning, TEXT("BadCount = %i"), c);
+ 	if ( a > b && a > c )
+ 	{
+ 		UE_LOG(LogTemp, Warning, TEXT("GoodCount = %i"), a);
+ 		MoodResult = WorkersMood::Good;
+ 	}			
+ 	else if ( b > a && b > c )
+ 	{
+ 		UE_LOG(LogTemp, Warning, TEXT("NormalCount = %i"), b);
+ 		MoodResult = WorkersMood::Normal;
+ 	}		
+ 	else if ( c > a && c > b )
+ 	{
+ 		UE_LOG(LogTemp, Warning, TEXT("BadCount = %i"), c);
 		MoodResult = WorkersMood::Bad;
-	}			
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Values are not unique"));
-	}
-	return MoodResult;
-}
-
+ 	}			
+ 	else
+ 	{
+ 		UE_LOG(LogTemp, Warning, TEXT("Values are not unique"));
+ 	}
+	GoodCount = 0;
+	NormalCount = 0;
+	BadCount = 0;
+ 	return MoodResult;
+ }
 
 
